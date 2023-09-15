e39da0a1827dba868d83e4e23c8afa2a22db246f
Ali Aydın <ayddinali@gmail.com>
2023-09-15 17:32:09 +0300

    EntityBase ve Entityler oluşturuldu.

---
+++ Core/NetCore7WebAPI.Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using NetCore7WebAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore7WebAPI.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public Product()
+        {
+                
+        }
+        public Product(string title,string description,decimal price,decimal discount,int brandId)
+        {
+            Title = title;
+            Description = description;
+            Price = price;
+            Discount = discount;
+            BrandId = brandId;
+        }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        //public required string ImagePath { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }
+        public required int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
