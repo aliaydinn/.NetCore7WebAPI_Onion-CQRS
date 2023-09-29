f974e5513c8879025f15f37d6cc22ba6fd5b5af7
Ali Aydın <ayddinali@gmail.com>
2023-09-29 16:46:16 +0300

    Generic Repository - ReadRepository

--- Infrastructure/NetCore7WebAPI.Persistence/Registration.cs
+++ Infrastructure/NetCore7WebAPI.Persistence/Registration.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetCore7WebAPI.Application.Interfaces.Repositories;
 using NetCore7WebAPI.Persistence.Context;
+using NetCore7WebAPI.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,8 @@ namespace NetCore7WebAPI.Persistence
         public static void AddPersistence(this IServiceCollection services,IConfiguration config)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
         }
     }
 }
