a21b9a0cc5b6a4061a01490faf4791e5db682abd
Ali Aydın <ayddinali@gmail.com>
2023-10-05 15:49:47 +0300

    UnitOfWork yapısı kuruldu.

---
+++ Infrastructure/NetCore7WebAPI.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using NetCore7WebAPI.Application.Interfaces.Repositories;
+using NetCore7WebAPI.Application.Interfaces.UnitOfWorks;
+using NetCore7WebAPI.Persistence.Context;
+using NetCore7WebAPI.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore7WebAPI.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await appDbContext.DisposeAsync();
+        }
+
+        public int Save()
+        {
+            return appDbContext.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await appDbContext.SaveChangesAsync();
+        }
+
+        IReadRepository<T> IUnitOfWork.GetReadRepository<T>()
+        {
+            return new ReadRepository<T>(appDbContext);
+        }
+
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>()
+        {
+            return new WriteRepository<T>(appDbContext);
+        }
+    }
+}
