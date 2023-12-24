6de11f7cafe638cf5a6c7a85d85775b0655aaa7f
Ali Aydın <ayddinali@gmail.com>
2023-12-24 19:01:49 +0300

    RefreshToken

---
+++ Core/NetCore7WebAPI.Application/Features/Auth/Command/RefreshToken/RefreshTokenCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using NetCore7WebAPI.Application.Bases;
+using NetCore7WebAPI.Application.Features.Auth.Rules;
+using NetCore7WebAPI.Application.Interfaces.AutoMapper;
+using NetCore7WebAPI.Application.Interfaces.Tokens;
+using NetCore7WebAPI.Application.Interfaces.UnitOfWorks;
+using NetCore7WebAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore7WebAPI.Application.Features.Auth.Command.RefreshToken
+{
+    public class RefreshTokenCommandHandler : BaseHandler , IRequestHandler<RefreshTokenCommandRequest,RefreshTokenCommandResponse>
+    {
+
+        private readonly UserManager<User> userManager;
+        private readonly ITokenService tokenService;
+        private readonly AuthRules authRules;
+
+        public RefreshTokenCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor, UserManager<User> userManager, ITokenService tokenService, AuthRules authRules) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.userManager = userManager;
+            this.tokenService = tokenService;
+            this.authRules = authRules;
+        }
+
+        public async Task<RefreshTokenCommandResponse> Handle(RefreshTokenCommandRequest request, CancellationToken cancellationToken)
+        {
+            ClaimsPrincipal? principle = tokenService.GetPrincipalFromExpriedToken(request.AccessToken);
+            string email = principle.FindFirstValue(ClaimTypes.Email);
+
+
+            User? user = await userManager.FindByEmailAsync(email);
+            IList<string> roles = await userManager.GetRolesAsync(user);
+
+            await authRules.RefreshTokenShouldNotBeExpired(user.RefreshTokenExpriyTime);
+
+            JwtSecurityToken newAccessToken =await tokenService.CreateToken(user, roles);
+            string newRefreshToken = tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            await userManager.UpdateAsync(user);
+
+            return new()
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                RefreshToken = newRefreshToken
+            };
+
+
+        }
+    }
+}
--- Core/NetCore7WebAPI.Application/Features/Auth/Rules/AuthRules.cs
+++ Core/NetCore7WebAPI.Application/Features/Auth/Rules/AuthRules.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace NetCore7WebAPI.Application.Features.Auth.Rules
 {
-    public class AuthRules :BaseRules
+    public class AuthRules : BaseRules
     {
         public Task TheUserMustNotExist(User? user)
         {
@@ -18,9 +18,15 @@ namespace NetCore7WebAPI.Application.Features.Auth.Rules
 
         }
 
-       public Task EmailorPasswordShouldBeInvalid(User? user, bool checkPassword)
+        public Task EmailorPasswordShouldBeInvalid(User? user, bool checkPassword)
         {
             if (user is null || !checkPassword) throw new EmailorPasswordShouldBeInvalidException();
+            return Task.CompletedTask;
+        }
+
+        public Task RefreshTokenShouldNotBeExpired(DateTime? expiredTime)
+        {
+            if (expiredTime <= DateTime.Now) throw new RefreshTokenShouldNotBeExpiredException();
             return Task.CompletedTask;
         }
     }
